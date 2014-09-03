c021d409fd2f4bee4b1d58d723b26fc00f23d4f0
Hector Castillo <h.j.castillo86@gmail.com>
2014-09-03 12:34:52 -0500

    Update FizzBuzz.java
    fixxed for loop because i'm dumb

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,7 +1,7 @@
 public class FizzBuzz{
 	public static void main (String[] args){
 		
-		for(int i=0;i<101;i++){
+		for(int i=1;i<101;i++){
 			if(i%15==0)
 				System.out.println("FizzBuzz");
 			else if (i%3==0)
